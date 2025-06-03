25ef8f6a000081086b6c9cfc38567fd46bc66f5a
Dystopian57 <fjcn1994@yahoo.es>
2025-06-04 00:04:01 +0200

    Lab Scripting Functional, Pre-merge Rivalname and CraftFeature

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -265,7 +265,7 @@
 #define VAR_SCOTT_BF_CALL_STEP_COUNTER                   0x40F5
 #define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER             0x40F6
 #define VAR_LITTLEROOT_PHASE                             0x40F7 // Used to define phases of Littleroot
-#define VAR_UNUSED_0x40F8                                0x40F8 // Unused Var
+#define VAR_CLOCK_SET                                    0x40F8 // Used to prevent player from leaving bedroom without setting the clock
 #define VAR_UNUSED_0x40F9                                0x40F9 // Unused Var
 #define VAR_UNUSED_0x40FA                                0x40FA // Unused Var
 #define VAR_UNUSED_0x40FB                                0x40FB // Unused Var
